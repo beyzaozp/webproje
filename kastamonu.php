8854d16afceb48e4c8a3a4973a165fb2c7c9d8d3
beyzaozp <beyzamunireozpinar@gmail.com>
2020-09-28 19:19:30 +0300

    Projrcet pertama

---
+++ kastamonu.php
@@ -0,0 +1,235 @@
+<!doctype html>
+<html lang="tr">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="bootstrap.css" >
+
+    <title>Kastamonu için tahminler-Weather Agency</title>
+  </head>
+  <body style="background-color:  #e6fbff">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="main.html">Weather Agency</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="main.php">Ana Sayfa <span class="sr-only">(current)</span></a>
+      </li>
+      
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Şehir Seç
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="istanbul.php">İstanbul</a>
+          <a class="dropdown-item" href="ankara.php">Ankara</a>
+          <a class="dropdown-item" href="kastamonu.php">Kastamonu</a>
+          <div class="dropdown-divider"></div>
+        </div>
+      </li>
+    </ul>
+
+  </div>
+</nav>
+      
+  <div class="row">
+    <div class="col ">
+     <div class="card ">
+  <a href=https://www.mgm.gov.tr/sondurum/turkiye.aspx><img src="img-1.png" class="card-img-top" alt="..."></a>
+  <div class="card-body">
+      <h5>Meteoroloji Genel Müdürlüğü sabah saatlerinde Türkiye'de meydana gelecek hava durumunu açıkladı. Yapılan değerlendirmelerinin yanı sıra il il uyarı yapıldı ve kuvvetli, yer yer gök gürültülü sağanak yağış geleceği belirtildi. </h5>
+    <p class="card-text">Açıklanan tahminlere göre, "Ülkemizin doğu kesimlerinin çok bulutlu, Doğu Karadeniz kıyıları, Doğu Anadolu’nun güneydoğusu, Güneydoğu Anadolu’nun doğusu ile Muş, Ağrı, Iğdır ve Şanlıurfa çevrelerinin sağanak ve yer yer gök gürültülü sağanak yağışlı, diğer yerlerin parçalı ve az bulutlu geçeceği tahmin ediliyor." denildi.
+
+Ayrıca hava sıcaklığı ve rüzgarla ilgili olarak, "Önemli bir değişiklik olmayacağı ve mevsim normalleri civarında seyredeceği tahmin ediliyor.Genellikle kuzeyli yönlerden, zamanla Akdeniz ve Güneydoğu Anadolu'da güney ve güneydoğu yönlerden hafif, ara sıra orta kuvvette eseceği tahmin ediliyor." ifadeleri yer aldı.</p>
+  </div>
+</div>
+    </div>
+<div class="col-6">
+        <h2 class="text-success">KASTAMONU İÇİN SAATLİK TAHMİN</h2>
+      <table class="table table-hover">
+  <thead>
+    <tr>
+      <th scope="col">Saat</th>
+      <th scope="col">Beklenen Hadise</th>
+      <th scope="col">Sıcaklık (°C)</th>
+      <th scope="col">Hissedilen Sıcaklık (°C)</th>
+        <th scope="col">Nem (%)</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">PERŞEMBE<br>
+18.00 - 21.00</th>
+      <td><img src="p.bulut.jpg"></td>
+      <td class="text-danger"style="font-weight: bold;">12</td>
+      <td class="text-warning"style="font-weight: bold;">12</td>
+        <td class="text-primary"style="font-weight: bold;">56</td>
+    </tr>
+    <tr>
+      <th scope="row">CUMA<br>
+21.00 - 24.00</th>
+      <td><img src="p.bulut.jpg"></td>
+      <td class="text-danger"style="font-weight: bold;">11</td>
+      <td class="text-warning"style="font-weight: bold;">11</td>
+        <td class="text-primary"style="font-weight: bold;">59</td>
+    </tr>
+    <tr>
+      <th scope="row">CUMA<br>
+24.00 - 03.00</th>
+      <td><img src="acık.jpg"></td>
+    <td class="text-danger"style="font-weight: bold;">20</td>
+        <td class="text-warning"style="font-weight: bold;">20</td>
+        <td class="text-primary"style="font-weight: bold;">41</td>
+   
+    </tr>
+      <tr>
+      <th scope="row">CUMA<br>
+03.00 - 06.00</th>
+      <td ><img src="acık.jpg"></td>
+        <td class="text-danger"style="font-weight: bold;">27</td>
+          <td class="text-warning"style="font-weight: bold;">26</td>
+          <td class="text-primary"style="font-weight: bold;">27</td>
+    </tr>
+      <tr>
+      <th scope="row">CUMA<br>
+06.00 - 09.00</th>
+      <td ><img src="acık.jpg"></td>
+        <td class="text-danger"style="font-weight: bold;">30</td>
+          <td class="text-warning"style="font-weight: bold;">28</td>
+          <td class="text-primary"style="font-weight: bold;">24</td>
+    </tr>
+      <tr>
+      <th scope="row">CUMA<br>
+09.00 - 12.00</th>
+      <td ><img src="acık.jpg"></td>
+          <td class="text-danger"style="font-weight: bold;">25</td>
+           <td class="text-warning"style="font-weight: bold;">22</td>
+          <td class="text-primary"style="font-weight: bold;">21</td>
+    </tr>
+      <tr>
+      <th scope="row">CUMA<br>
+12.00 - 15.00</th>
+      <td ><img src="acık.jpg"></td>
+          <td class="text-danger"style="font-weight: bold;">33</td>
+          <td class="text-warning"style="font-weight: bold;">32</td>
+          <td class="text-primary"style="font-weight: bold;">49</td>
+    </tr>
+      <tr>
+      <th scope="row">CUMA<br>
+15.00 - 18.00</th>
+      <td ><img src="acık.jpg"></td>
+          <td class="text-danger"style="font-weight: bold;">20</td>
+      <td class="text-warning"style="font-weight: bold;">20</td>
+          <td class="text-primary"style="font-weight: bold;">49</td>
+    </tr>
+      <tr>
+      <th scope="row">CUMA<br>
+18.00 - 21.00</th>
+      <td ><img src="ac%C4%B1k.jpg"></td>
+      <td class="text-danger"style="font-weight: bold;">18</td>
+      <td class="text-warning" style="font-weight: bold;">18</td>
+          <td class="text-primary"style="font-weight: bold;">54</td>
+    </tr>
+      <tr>
+      <th scope="row">CUMA<br>
+21.00 - 24.00</th>
+      <td ><img src="ac%C4%B1k.jpg"></td>
+          <td class="text-danger"style="font-weight: bold;">6</td>
+      <td class="text-warning"style="font-weight: bold;">6</td>
+          <td class="text-primary"style="font-weight: bold;">5</td>
+    </tr>
+  </tbody>
+</table>
+        
+        
+        
+        <h2 class="text-success">5 GÜNLÜK TAHMİN</h2>
+      <table class="table table-hover">
+  <thead >
+    <tr>
+      <th scope="col">TARİH</th>
+      <th scope="col">Beklenen Hadise</th>
+      <th scope="col">Sıcaklık (En Düşük) (°C)</th>
+      <th scope="col">Sıcaklık (En Yüksek) (°C)</th>
+        <th scope="col">Nem (En Düşük) (%)</th>
+        <th scope="col">Nem (En Yüksek) (%)</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">15 Mayıs Cuma<br></th>
+      <td><img src="ac%C4%B1k.jpg"></td>
+      <td class="text-info" style="font-weight: bold;">12</td>
+      <td class="text-danger" style="font-weight: bold;">31</td>
+        <td class="text-warning" style="font-weight: bold;">25</td>
+        <td class="text-primary" style="font-weight: bold;">60</td>
+    </tr>
+    <tr>
+      <th scope="row">16 Mayıs Cumartesi<br></th>
+      <td><img src="ac%C4%B1k.jpg"></td>
+      <td class="text-info" style="font-weight: bold;">11</td>
+      <td class="text-danger" style="font-weight: bold;">32</td>
+        <td class="text-warning" style="font-weight: bold;">22</td>
+        <td class="text-primary" style="font-weight: bold;">93</td>
+    </tr>
+    <tr>
+      <th scope="row">17 Mayıs Pazar<br></th>
+      <td><img src="ac%C4%B1k.jpg"></td>
+    <td class="text-info" style="font-weight: bold;">11</td>
+        <td class="text-danger" style="font-weight: bold;">29</td>
+        <td class="text-warning" style="font-weight: bold;">30</td>
+        <td class="text-primary" style="font-weight: bold;">69</td>
+   
+    </tr>
+      <tr>
+      <th scope="row">18 Mayıs Pazartesi<br></th>
+      <td ><img src="acık.jpg"></td>
+        <td class="text-info"style="font-weight: bold;">13</td>
+          <td class="text-danger"style="font-weight: bold;">29</td>
+          <td class="text-warning" style="font-weight: bold;">30</td>
+          <td class="text-primary" style="font-weight: bold;">69</td>
+    </tr>
+      <tr>
+      <th scope="row">19 Mayıs Salı<br></th>
+      <td ><img src="acık.jpg"></td>
+        <td class="text-info"style="font-weight: bold;">11</td>
+          <td class="text-danger"style="font-weight: bold;">33</td>
+          <td class="text-warning" style="font-weight: bold;">21</td>
+          <td class="text-primary" style="font-weight: bold;">84</td>
+        </tr>
+  </tbody>
+</table>
+        
+    </div>
+    <div class="col">
+      <div class="card" >
+  <img src="haber-5.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Koronavirüs Dünya Haritası (CANLI)</h5>
+    <p class="card-text">Corona virüs haritası CANLI</p><?php
+    date_default_timezone_set('Europe/Istanbul');
+      echo date('d.m.Y ');
+    ?><br>
+      
+    <a href="https://corona.cbddo.gov.tr" class="btn btn-primary">Siteye Git</a>
+  </div>
+          
+</div>
+    </div>
+  </div>
+      
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+          <script language="javascript" src="http://ir.sitekodlari.com/yukaricik1.js"></script>
+  </body>
+</html>
---
+++ main.php
@@ -0,0 +1,188 @@
+    <!doctype html>
+<html lang="tr">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="bootstrap.css" >
+
+    <title>Weather Agency</title>
+  </head>
+  <body style="background-color:  #e6fbff" >
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="main.html">Weather Agency</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="main.php">Ana Sayfa <span class="sr-only">(current)</span></a>
+      </li>
+      
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Şehir Seç
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="istanbul.php">İstanbul</a>
+          <a class="dropdown-item" href="ankara.php">Ankara</a>
+          <a class="dropdown-item" href="kastamonu.php">Kastamonu</a>
+        </div>
+      </li>
+    </ul>
+    
+  </div>
+</nav>
+      
+  <div class="row">
+    <div class="col ">
+     <div class="card ">
+  <img src="re1.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+      <h5>Son dakika: O illerimizde 44 dereceye çıkacak! Meteoroloji'den flaş hava durumu, sıcak hava uyarısı</h5>
+      <p class="card-text">Türkiye'de bu hafta sonu Marmara'nın güneyi, Ege, Akdeniz ve İç Anadolu bölgeleri ile Batı ve Orta Karadeniz'in iç kesimlerinde yükselecek hava sıcaklıklarıyla "son yılların en sıcak mayıs ayının" yaşanabileceği tahmin ediliyor.</p>
+  </div>
+</div>
+    </div>
+    <div class="col-6">
+  <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="haber2.jpeg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5  class="text-light bg-dark" class="font-weight-bold">Son dakika: O illerimizde 44 dereceye çıkacak! Meteoroloji'den flaş hava durumu, sıcak hava uyarısı</h5>
+          <a href="https://www.sabah.com.tr/yasam/2020/04/12/meteorolojiden-son-dakika-saganak-yagis-ve-hava-durumu-uyarisi-yarin-hava-nasil-olacak"><p>Haberin Devamı için Tıklayın</p></a>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img2.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5 class="text-light bg-dark" class="font-weight-bold">Dünyada Bazı Merkezlere Ait 3 Günlük Hava Tahmini </h5>
+        <a href="https://tr.euronews.com/hava-durumu"><p class="text-light bg-dark" class="font-weight-bold">Öğrenmek için Tıklayın</p></a>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="haber-6.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5 class="text-light bg-dark">Hava durumu nasıl olacak? Meteorolojiden dikkat çeken uyarı!</h5>
+          <a href="https://www.cnnturk.com/turkiye/hava-durumu-13-nisan-yurt-genelinde-bahar-havasi-yagis-yok" class="text-light bg-dark">Öğrenmek için Tıklayın</a>
+      </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+        <h2 class="text-info">DENİZLERDE HAVA</h2>
+        <p>Fırtına beklenmiyor</p>
+        <h4 class="text-danger">KARADENİZ</h4>
+        <p>Hava Durumu: Az bulutlu, Batı ve Orta Karadeniz yer yer sis ve puslu, Rüzgar: Batı Karadeniz’de güney ve güneybatıdan 3 ila 5 (20-40 km/sa); Doğu Karadeniz’de doğu ve güneydoğudan 2 ila 4 (10-30 km/sa) yer yer 5kuvvetinde; Dalga: 0,5 ila 1,5 m, Görüş: İyi, sis anında zayıf.</p>
+        
+        <h4 class="text-danger">MARMARA</h4>
+        <p>Hava Durumu: Az bulutlu; yer yer sis ve puslu Rüzgar: Güney ve güneybatıdan 2 ila 4 zamanla 3 ila 5 (20-40 km/sa) kuvvetinde; Dalga: 0,5 ila 1,5m; Görüş: İyi.</p>
+        
+        <h4 class="text-danger">EGE</h4>
+        <p>Hava Durumu: Az bulutlu ve açık, Rüzgar: Güney ve güneybatıdan 2 ila 4 (10-30km/sa), güneyinde kuzey ve kuzeybatıdan 3 ila 5 (20-40 km/sa) kuvvetinde; Dalga: 0,5 ila 1,5 m, Görüş: İyi.</p>
+        
+        <h4 class="text-danger">AKDENİZ</h4>
+        <p>Hava Durumu: Az bulutlu, Rüzgar: Batı Akdeniz’de batı ve güneybatıdan, kuzeyi kuzeydoğudan 2 ila 4 (10-30 km/sa); Doğu Akdeniz’de kuzey ve kuzeydoğudan, akşam güneybatıdan 2 ila 4 (10-30 km/sa) kuvvetinde, Dalga: 0,5 ila 1,5 m, Görüş: İyi.</p>
+        
+        <h4 class="text-danger">VAN GÖLÜ</h4>
+        <p>Hava Durumu: Parçalı az bulutlu, Rüzgar: Kuzeydoğu zamanla güneybatıdan 2 ila 4 (10-30 km/sa) kuvvetinde; Dalga: 0,25 ila 0,5m; Görüş: İyi.</p>
+        <!-- weather widget start --><div id="m-booked-custom-widget-46285"> <div class="weather-customize" style="width:350px;"> <div class="booked-weather-custom-160 color-f8bd0d" style="width:350px;" id="width5"> <div class="booked-weather-custom-160-date">Hava Durumu, 14 Mayıs</div> <div class="booked-weather-custom-160-main more"> <a target="_blank" href="https://bookeder.com/weather/istanbul-18319" class="booked-weather-custom-160-city"> İstanbul Hava Durumu </a> <a target="_blank" class="booked-wzs-bottom-custom-160" href="https://www.booked.net/"><img src="//s.bookcdn.com/images/letter/s5.gif" alt="booked.net" /></a> <div class="booked-weather-custom-160-degree booked-weather-custom-C wmd03"><span><span class="plus">+</span>20</span></div> <div class="booked-weather-custom-details"> <p><span>Yük.: <strong><span class="plus">+</span>20<sup>°</sup></strong></span><span> Düş.: <strong><span class="plus">+</span>20<sup>°</sup></strong></span></p> <p>Nem: <strong>72%</strong></p> <p>Rüzgar: <strong>SSE - 6 KPH</strong></p> </div> </div> <div class="booked-weather-custom-160-main more"> <a target="_blank" href="https://bookeder.com/weather/kastamonu-47015" class="booked-weather-custom-160-city"> Kastamonu Hava Durumu </a> <div class="booked-weather-custom-160-degree booked-weather-custom-C wmd06"><span><span class="plus">+</span>17</span></div> <div class="booked-weather-custom-details"> <p><span>Yük.: <strong><span class="plus">+</span>17<sup>°</sup></strong></span><span> Düş.: <strong><span class="plus">+</span>16<sup>°</sup></strong></span></p> <p>Nem: <strong>63%</strong></p> <p>Rüzgar: <strong>SW - 4 KPH</strong></p> </div> </div> <div class="booked-weather-custom-160-main more"> <a target="_blank" href="https://bookeder.com/weather/ankara-18522" class="booked-weather-custom-160-city"> Ankara Hava Durumu </a> <div class="booked-weather-custom-160-degree booked-weather-custom-C wmd01"><span><span class="plus">+</span>20</span></div> <div class="booked-weather-custom-details"> <p><span>Yük.: <strong><span class="plus">+</span>20<sup>°</sup></strong></span><span> Düş.: <strong><span class="plus">+</span>19<sup>°</sup></strong></span></p> <p>Nem: <strong>49%</strong></p> <p>Rüzgar: <strong>W - 3 KPH</strong></p> </div> </div> <div class="booked-weather-custom-160-main more"> <a target="_blank" href="https://bookeder.com/weather/izmir-18523" class="booked-weather-custom-160-city"> İzmir Hava Durumu </a> <div class="booked-weather-custom-160-degree booked-weather-custom-C wmd01"><span><span class="plus">+</span>22</span></div> <div class="booked-weather-custom-details"> <p><span>Yük.: <strong><span class="plus">+</span>23<sup>°</sup></strong></span><span> Düş.: <strong><span class="plus">+</span>22<sup>°</sup></strong></span></p> <p>Nem: <strong>53%</strong></p> <p>Rüzgar: <strong>NW - 5 KPH</strong></p> </div> </div> <div class="booked-weather-custom-160-main more"> <a target="_blank" href="https://bookeder.com/weather/antalya-893" class="booked-weather-custom-160-city"> Antalya Hava Durumu </a> <div class="booked-weather-custom-160-degree booked-weather-custom-C wmd01"><span><span class="plus">+</span>24</span></div> <div class="booked-weather-custom-details"> <p><span>Yük.: <strong><span class="plus">+</span>24<sup>°</sup></strong></span><span> Düş.: <strong><span class="plus">+</span>24<sup>°</sup></strong></span></p> <p>Nem: <strong>73%</strong></p> <p>Rüzgar: <strong>ENE - 2 KPH</strong></p> </div> </div> </div></div> </div><script type="text/javascript"> var css_file=document.createElement("link"); css_file.setAttribute("rel","stylesheet"); css_file.setAttribute("type","text/css"); css_file.setAttribute("href",'https://s.bookcdn.com/css/weather.css?v=0.0.1'); document.getElementsByTagName("head")[0].appendChild(css_file); function setWidgetData(data) { if(typeof(data) != 'undefined' && data.results.length > 0) { for(var i = 0; i < data.results.length; ++i) { var objMainBlock = document.getElementById('m-booked-custom-widget-46285'); if(objMainBlock !== null) { var copyBlock = document.getElementById('m-bookew-weather-copy-'+data.results[i].widget_type); objMainBlock.innerHTML = data.results[i].html_code; if(copyBlock !== null) objMainBlock.appendChild(copyBlock); } } } else { alert('data=undefined||data.results is empty'); } } </script> <script type="text/javascript" charset="UTF-8" src="https://widgets.booked.net/weather/info?action=get_weather_info&ver=6&cityID=18319,47015,18522,18523,893&type=2&scode=124&ltid=3458&domid=765&anc_id=44559&cmetric=1&wlangID=21&color=f8bd0d&wwidth=350&header_color=ffffff&text_color=333333&link_color=08488D&border_form=1&footer_color=ffffff&footer_text_color=333333&transparent=0"></script><!-- weather widget end -->
+
+        
+        
+    </div>
+      
+    <div class="col">
+        <!-- clock widget start -->
+<iframe scrolling="no" frameborder="no" clocktype="html5" style="overflow:hidden;border:0;margin:0;padding:0;width:240px;height:80px;"src="https://www.clocklink.com/html5embed.php?clock=043&timezone=Turkey_Ankara&color=green&size=240&Title=&Message=&Target=&From=2020,1,1,0,0,0&Color=green"></iframe>
+         
+        <h2 class="text-info">METEOROLOJİK GÖRÜNÜM</h2> 
+        <p>Meteoroloji Genel Müdürlüğü tarafından yapılan son değerlendirmelere göre; Ülkemiz genelinde yağış beklenmiyor. Yurdun doğu kesimlerinin parçalı ve yer yer çok bulutlu, diğer yerlerin az bulutlu ve açık geçeceği tahmin ediliyor.</p>
+        
+    <h2 class="text-info">BÖLGELERİMİZDE HAVA</h2>
+        <h4 class="text-danger">MARMARA</h4>
+        <p> Az bulutlu ve açık geçeceği tahmin ediliyor.</p>
+        <p class="font-weight-bold">EDİRNE <span class="text-success">16°C</span>, <span class="text-danger">36°C </span><p><p>Az bulutlu ve açık</p>     
+        <p class="font-weight-bold">İSTANBUL <span class="text-success">16°C</span>, <span class="text-danger">28°C </span><p><p>Az bulutlu ve açık</p>
+        <p class="font-weight-bold">KIRKLARELİ <span class="text-success">15°C</span>, <span class="text-danger"330°C </span><p><p>Az bulutlu ve açık</p>
+        
+        
+        <h4 class="text-danger">EGE</h4>
+        <p> Az bulutlu ve açık geçeceği tahmin ediliyor.</p>
+        <p class="font-weight-bold">A.KARAHİSAR <span class="text-success">3°C</span>, <span class="text-danger">19°C </span><p><p>Az bulutlu</p>    
+        <p class="font-weight-bold">DENİZLİ <span class="text-success">10°C</span>, <span class="text-danger">22°C </span><p><p>Az bulutlu</p>
+        <p class="font-weight-bold">İZMİR <span class="text-success">8°C</span>, <span class="text-danger">22°C </span><p><p>Az bulutlu ve açık</p>
+
+        
+        <h4 class="text-danger">İÇ ANADOLU</h4>
+        <p> Az bulutlu ve açık geçeceği tahmin ediliyor.</p>
+        <p class="font-weight-bold">ANKARA  <span class="text-success">21°C</span>, <span class="text-danger">37°C </span><p><p>Az bulutlu ve açık</p>   
+        <p class="font-weight-bold">ÇANKIRI  <span class="text-success">21°C</span>, <span class="text-danger">37°C </span><p><p>Az bulutlu ve açık</p>
+        <p class="font-weight-bold">ESKİŞEHİR  <span class="text-success">20°C</span>, <span class="text-danger">39°C </span><p><p>Az bulutlu ve açık</p>
+        
+        
+        <h4 class="text-danger">AKDENİZ</h4>
+        <p> Az bulutlu ve açık geçeceği tahmin ediliyor.</p>
+        <p class="font-weight-bold">ADANA <span class="text-success">20°C</span>, <span class="text-danger">38°C </span><p><p>Az bulutlu ve açık</p>
+        <p class="font-weight-bold">ANTALYA <span class="text-success">18°C</span>, <span class="text-danger">35°C </span><p><p>Az bulutlu ve açık</p>
+        <p class="font-weight-bold">HATAY <span class="text-success">18°C</span>, <span class="text-danger">37°C </span><p><p>Az bulutlu ve açık</p>
+        
+        
+        <h4 class="text-danger">BATI KARADENİZ</h4>
+        <p>Az bulutlu ve açık geçeceği tahmin ediliyor.</p>
+        <p class="font-weight-bold">BOLU  <span class="text-success">13°C</span>, <span class="text-danger">31°C </span><p><p>Parçalı ve az bulutlu</p>   
+        <p class="font-weight-bold">DÜZCE  <span class="text-success">16°C</span>, <span class="text-danger">34°C </span><p><p>Parçalı ve az bulutlu</p>
+        <p class="font-weight-bold">KASTAMONU  <span class="text-success">11°C</span>, <span class="text-danger">33°C </span><p><p>Parçalı ve az bulutlu</p>
+        
+        
+        <h4 class="text-danger">ORTA ve DOĞU KARADENİZ</h4>
+        <p>Parçalı ve az bulutlu, Doğu Karadeniz'in yer yer çok bulutlu geçeceği tahmin ediliyor.</p>
+        <p class="font-weight-bold">GÜMÜŞHANE  <span class="text-success">17°C</span>, <span class="text-danger">26°C </span><p><p>Az bulutlu ve açık</p>   
+        <p class="font-weight-bold">SAMSUN  <span class="text-success">14°C</span>, <span class="text-danger">31°C </span><p><p>Az bulutlu ve açık</p>
+        <p class="font-weight-bold">TOKAT  <span class="text-success">15°C</span>, <span class="text-danger">24°C </span><p><p>Parçalı ve az bulutlu</p>
+        
+        
+        <h4 class="text-danger">DOĞU ANADOLU</h4>
+        <p>Az bulutlu ve açık, bölgenin kuzeydoğu kesimlerinin parçalı bulutlu geçeceği tahmin ediliyor.</p>
+        <p class="font-weight-bold">ERZURUM <span class="text-success">6°C</span>, <span class="text-danger">23°C </span><p><p>Parçalı ve az bulutlu</p>   
+        <p class="font-weight-bold">KARS  <span class="text-success">0°C</span>, <span class="text-danger">22°C </span><p><p>Parçalı ve az bulutlu</p>
+        <p class="font-weight-bold">MALATYA <span class="text-success">14°C</span>, <span class="text-danger">30°C </span><p><p>Az bulutlu ve açık</p>
+        
+        
+        <h4 class="text-danger">GÜNEYDOĞU ANADOLU</h4>
+        <p>Az bulutlu ve açık geçeceği tahmin ediliyor.</p>
+        <p class="font-weight-bold">DİYARBAKIR <span class="text-success">14°C</span>, <span class="text-danger">30°C </span><p><p>Az bulutlu ve açık</p>   
+        <p class="font-weight-bold">GAZİANTEP <span class="text-success">19C</span>, <span class="text-danger">28°C </span><p><p>Az bulutlu ve açık</p>
+        <p class="font-weight-bold">SİİRT <span class="text-success">16°C</span>, <span class="text-danger">29°C </span><p><p>Az bulutlu ve açık</p>
+        
+        
+        
+    </div>
+  </div>
+
+      
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+          <script language="javascript" src="http://ir.sitekodlari.com/yukaricik1.js"></script>
+  </body>
+</html>
